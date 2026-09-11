9ad200380bc8fc764a3ca8bceb1fef724ee2c3b5
dahool <dahool@users.noreply.github.com>
2026-09-11 17:14:23 -0300

    update immich v3.2

--- sql/01_person_to_album.sql
+++ sql/01_person_to_album.sql
@@ -1,4 +1,3 @@
-
 CREATE OR REPLACE FUNCTION public.person_assets_to_album_function()
     RETURNS trigger
     LANGUAGE 'plpgsql'
@@ -6,18 +5,32 @@ CREATE OR REPLACE FUNCTION public.person_assets_to_album_function()
     VOLATILE NOT LEAKPROOF
 AS $BODY$
 BEGIN
-    IF NEW."personId" IS NOT NULL THEN
-        PERFORM insert_assets_for_person(NEW."personId");
+    IF (TG_OP = 'INSERT') THEN
+        IF NEW."personGroupId" IS NOT NULL THEN
+            PERFORM insert_assets_for_person(NEW."personGroupId");
+        END IF;
+        
+    ELSIF (TG_OP = 'UPDATE') THEN
+        -- Only run if the personGroupId was actually changed
+        IF NEW."personGroupId" IS DISTINCT FROM OLD."personGroupId" THEN
+            IF NEW."personGroupId" IS NOT NULL THEN
+                PERFORM insert_assets_for_person(NEW."personGroupId");
+            END IF;
+            IF OLD."personGroupId" IS NOT NULL THEN
+                PERFORM insert_assets_for_person(OLD."personGroupId");
+            END IF;
+        END IF;
     END IF;
+    
     RETURN NEW;
 END;
 $BODY$;
 
 ALTER FUNCTION public.person_assets_to_album_function()
     OWNER TO postgres;
-    
+
 CREATE OR REPLACE TRIGGER person_assets_to_album
     AFTER INSERT OR UPDATE 
     ON public.asset_face
     FOR EACH ROW
-    EXECUTE FUNCTION public.person_assets_to_album_function();
+    EXECUTE FUNCTION public.person_assets_to_album_function();    
